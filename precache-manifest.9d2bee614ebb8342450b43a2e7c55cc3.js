9421e34ebdf3954a965072c9b7de80eaeb28342b
Magnus Lindeberg <magnus.lindeberg@ottoboni.se>
2019-01-23 12:23:49 +0100

    Updates

---
+++ precache-manifest.9d2bee614ebb8342450b43a2e7c55cc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "844d2e79dfff6ff3a6c2",
+    "url": "/static/css/main.24f8b679.chunk.css"
+  },
+  {
+    "revision": "844d2e79dfff6ff3a6c2",
+    "url": "/static/js/main.844d2e79.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc714a1ecb7b3c06e5439e8f3d624c3e",
+    "url": "/index.html"
+  }
+];

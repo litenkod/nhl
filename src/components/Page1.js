154bb195fcaba847daf71c8f29833b22f67f3c71
Magnus Lindeberg <magnus.lindeberg@ottoboni.se>
2019-01-25 09:47:45 +0100

    added CNAME file and update homepage

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -15,13 +15,17 @@ export class Page1 extends React.Component {
         };
     }
 
-    updateID = (event, ID) =>{
+    selectTeam = (event, ID) =>{
         this.setState({
             teamId: ID
         });
+
+        console.log('this: ', this);
+        console.log('window: ', window);
+        // window.scrollTo(0, this.myRef.current.offsetTop)
     }
 
-    updateSort = (event, sortType) =>{
+    updateSortOrder = (event, sortType) =>{
         this.setState({
             teamSort: sortType
         });
@@ -36,8 +40,8 @@ export class Page1 extends React.Component {
                 <img src="https://www-league.nhlstatic.com/images/logos/league-dark/133.svg" alt="NHL"/>
                 <h1>NHL 2018/19</h1>
             </section>
-            <GetTeams updateTeamID={this.updateID.bind(null, 1)} temaId={this.state.teamId} />
-            <SortList updateSortType={this.updateSort.bind(null, 1)} teamSort={this.state.teamSort} />
+            <GetTeams updateTeamID={this.selectTeam.bind(null, 1)} temaId={this.state.teamId} />
+            <SortList updateSortType={this.updateSortOrder.bind(null, 1)} teamSort={this.state.teamSort} />
             <GetRoster teamId={this.state.teamId} teamSort={this.state.teamSort}/>
         </div>
         );
